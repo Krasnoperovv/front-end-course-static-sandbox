449e74139df3902480c89074c5e24df6d4435a3e
Krasnoperovv <egor@manychat.com>
2018-11-25 14:05:15 +0300

    ClickCounter_V1

--- src/clickCounter/script.js
+++ src/clickCounter/script.js
@@ -1,10 +1,12 @@
 const clickBtn = document.getElementById('click-btn')
 const clickValue = document.getElementById('click-value')
 const clickReset = document.getElementById('click-reset')
+const clickIncrease = document.getElementById('click-increase')
+const clickMax = document.getElementById('clickMax')
 
 let clicks = 0
 let step = 1
-const maxValue = 10
+const maxValue = 20
 
 clickBtn.onclick = function() {
 	if (clicks < maxValue) {
@@ -13,6 +15,24 @@ clickBtn.onclick = function() {
 		clicks = 'max value reached'
 	}
 
+	clickValue.innerHTML = clicks
+}
+
+clickIncrease.onclick = function() {
+	if (clicks < maxValue) {
+		clicks = 2 + clicks
+	} else {
+		clicks = 'max value reached'
+	}
+	clickValue.innerHTML = clicks
+}
+
+clickMax.onclick = function() {
+	if (clicks < maxValue) {
+		clicks = 20
+	} else {
+		clicks = 'max value reached'
+	}
 	clickValue.innerHTML = clicks
 }
 
