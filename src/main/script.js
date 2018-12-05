1ecfccc911a7ae40e0985195ae4cc75019613da0
Krasnoperovv <egor@manychat.com>
2018-12-05 19:26:19 +0300

    Comment out cards

--- src/main/script.js
+++ src/main/script.js
@@ -1,30 +1,30 @@
-const container = document.getElementById('container')
-const features = [
-	{ label: 'Broadcasts', icon: '🤯' },
-	{ label: 'Flows', icon: '🤡' },
-	{ label: 'Templates', icon: '🤡' },
-	{ label: 'Keywords', icon: '🤡' },
-]
+// const container = document.getElementById('container')
+// const features = [
+// 	{ label: 'Broadcasts', icon: '🤯' },
+// 	{ label: 'Flows', icon: '🤡' },
+// 	{ label: 'Templates', icon: '🤡' },
+// 	{ label: 'Keywords', icon: '🤡' },
+// ]
 
-const person = { age: 27, name: 'Dima' }
-console.log('person', person)
+// const person = { age: 27, name: 'Dima' }
+// console.log('person', person)
 
-// function greet(name) {
-// 	return 'Hello,' + name + '!'
-// }
+// // function greet(name) {
+// // 	return 'Hello,' + name + '!'
+// // }
 
-// function CanDrink(age) {
-// 	return age >= 18
-// }
-// const greeting = greet('Dima')
+// // function CanDrink(age) {
+// // 	return age >= 18
+// // }
+// // const greeting = greet('Dima')
 
-features.forEach(function(item) {
-	const feature = document.createElement('div')
-	const icon = document.createElement('div')
-	icon.innerHTML = item.icon
-	icon.classList.add('text-center')
+// features.forEach(function(item) {
+// 	const feature = document.createElement('div')
+// 	const icon = document.createElement('div')
+// 	icon.innerHTML = item.icon
+// 	icon.classList.add('text-center')
 
-	feature.classList.add('card', 'card-shadow', 'border')
-	feature.append(icon)
-	container.body.append(icon)
-})
+// 	feature.classList.add('card', 'card-shadow', 'border')
+// 	feature.append(icon)
+// 	container.body.append(icon)
+// })
